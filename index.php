5f178d471d18ae9e110397a845cdb3097dc8a74b
KonstantinShirmanov <45903009+KonstantinShirmanov@users.noreply.github.com>
2020-06-12 17:16:49 +0300

    Update index.php

--- index.php
+++ index.php
@@ -45,11 +45,11 @@
           
           // сверим полученный пароль с теми то хранятся в массиве $users
           if(isset($users[$login])) {
-             if($pwd == $users[$login]) {
+             if($pwd == $users[$login][$password]) {
               // установим переменные сесии
               $_SESSION["autorized"] = true;
               $_SESSION["user"] = $login;
-              $_SESSION["email"] =$value;
+              $_SESSION["email"] =$users[$login][$email];
             }
           }
         }
